736d8bb8d97399006851a8be8bf3eca57305d5c6
manivannancsmca <manivannancsmca@gmail.com>
2026-06-20 16:40:23 +0530

    first commit

---
+++ src/main/java/com/spring_cicd_reusable_workflow_another_repository/SpringCicdReusableWorkflowAnotherRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.spring_cicd_reusable_workflow_another_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdReusableWorkflowAnotherRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdReusableWorkflowAnotherRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_cicd_reusable_workflow_another_repository/SpringCicdReusableWorkflowAnotherRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_cicd_reusable_workflow_another_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdReusableWorkflowAnotherRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
